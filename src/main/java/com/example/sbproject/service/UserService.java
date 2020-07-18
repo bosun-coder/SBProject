0eae88356db4637cb39ac22cde00a7d4e0643cb1
bosun-coder <bosun0542desu@gmail.com>
2020-07-18 21:02:42 +0400

    create mailsendler

--- src/main/java/com/example/sbproject/service/UserService.java
+++ src/main/java/com/example/sbproject/service/UserService.java
@@ -1,17 +1,27 @@
 package com.example.sbproject.service;
 
+import com.example.sbproject.domain.Role;
+import com.example.sbproject.domain.User;
 import com.example.sbproject.repos.UserRepository;
+import freemarker.template.utility.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
 
     //@Autowired
     private final UserRepository userRepository;
+
+    @Autowired
+    private MailSender mailSender;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -20,5 +30,46 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return userRepository.findByUsername(s);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepository.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+
+        if (user == null) {
+            return false;
+        }
+
+        user.setActivationCode(null);
+
+        userRepository.save(user);
+
+        return true;
     }
 }
