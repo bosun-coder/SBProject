819a323945b26f529efc9948b3425fd785ddb810
bosun-coder <bosun0542desu@gmail.com>
2020-07-19 11:46:13 +0400

    migration and profile

--- src/main/java/com/example/sbproject/repos/UserRepository.java
+++ src/main/java/com/example/sbproject/repos/UserRepository.java
@@ -4,8 +4,8 @@ import com.example.sbproject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
 
+    User findByUsername(String username);
 
     User findByActivationCode(String code);
 }
